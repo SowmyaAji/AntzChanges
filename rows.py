94ad388d19e49a55d218fd89bc7831ad30baa882
Sowmya Aji <sowmya.aji@gmail.com>
2020-03-24 22:20:08 -0400

    Added topo_color

--- rows.py
+++ rows.py
@@ -60,12 +60,32 @@ def change_color(rows, selected_row, parent, color_r='0', color_g='255', color_b
         Sets the values for scale_x, scale_y and scale_z and parent_id based on input 
         and returns a CSV input with updated data
 
-        :params: Data dict, id of selected row, parent, scale_x, scale_y, scale)z
+        :params: Data dict, id of selected row, parent. Takes all the rest as defaults unless given as input.
         :returns: updated row for CSV file
     
     """
     extras = dict(color_r=color_r, color_g=color_g, color_b=color_b) 
     return change_function(rows, selected_row, extras, lambda row: row['parent_id'] == parent)
+
+
+
+def topo_color(rows, selected_row, geometry, topo = '4', translate_x='-90', translate_y='90', color_r='0', color_g='152', color_b='255'):
+    """
+        Sets the values topo, translate_x, translate_y, color_r, color_g, color_b with defaults or changed based on input 
+        and returns a CSV input with updated data
+
+        :params: Data dict, id of selected row, geometry. Takes all the rest as defaults unless given as input.
+        :returns: updated row for CSV file
+    
+    """
+    extras = dict(topo=top0, translate_x=translate_x, translate_y=translate_y, color_r=color_r, color_g=color_g, color_b=color_b)
+    return change_function(rows, selected_row, extras, lambda row: row['geometry'] == geometry)
+
+
+# Change all objects with geometry = 3 to have topo = 4, translate_x = -90,  translate_y = 90, and color_r = 0, color_g=152, color_b=255. (not including rows with id 1-6)
+
+
+
 
 def write_to_file(output_rows, out_file = 'chin1.csv'):
     with open(out_file, 'w', newline='') as f:
@@ -76,9 +96,9 @@ def write_to_file(output_rows, out_file = 'chin1.csv'):
 
 
 def main():
-    scale_list = scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
-    # write_to_file(scale_list)
-    write_to_file(change_color(scale_list, '36', '0'))
+    output_list = scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
+    output_list = change_color(output_list, '36', '0')
+    output_list = topo_color(output_list, '36', '3')
 
 
 if __name__ == "__main__":
