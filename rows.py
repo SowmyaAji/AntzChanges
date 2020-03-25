a28f8467d6044de5f27fc316e2b113c1dfd87e45
Sowmya Aji <sowmya.aji@gmail.com>
2020-03-24 21:34:51 -0400

    Add ability to change scale and color with defaults

--- rows.py
+++ rows.py
@@ -1,14 +1,13 @@
 import csv
 
 
-
-def get_rows():
+def get_rows(in_file = 'chin.csv'):
     """Open csv files and return the data"""
-    with open('chin.csv') as csvfile:
+    with open(in_file) as csvfile:
         rows = csv.DictReader(csvfile)
         return list(rows)
 
-def get_column_names():
+def get_column_names(in_file = 'chin.csv'):
     """
         Gets the column names as a single string to form the header
         
@@ -16,10 +15,24 @@ def get_column_names():
         :returns: column names as a string
     
     """
-    with open("chin.csv", 'r') as f:
-        columns = f.readline().strip()   
-        return str(columns)
-# get_column_names()
+    with open(in_file, 'r') as f:
+        return f.readline().strip().split(',')
+
+
+def change_function(rows, selected_row, extras={}, condition_function = lambda row: False):
+    """
+        Over all function that effects change in the rows    
+    """
+    try:
+        output = []
+        for row  in rows:
+            if row['id'] == selected_row and condition_function(row):
+                row.update(extras)
+            output.append(row)
+        print("Color output length", len(output))
+        return output
+    except ValueError:
+        print("This row does not exist")   
 
 def scale_xyz(rows, selected_row, parent, scale_x='0.4', scale_y='0.4', scale_z='0.4'):
     """
@@ -30,41 +43,19 @@ def scale_xyz(rows, selected_row, parent, scale_x='0.4', scale_y='0.4', scale_z=
         :returns: updated row for CSV file
     
     """
-    print("I AM AT SCALE XYZ")
-    print('IM ROW ONE', rows[0])
-    try:
-        output = list()
-        for row in rows:
-            if row['id'] == selected_row:
-                if parent == '0':
-                    row['id'] = selected_row
-                    row['parent_id'] = parent
-                    row['scale_x'] = scale_x
-                    row['scale_y'] = scale_y
-                    row['scale_z'] = scale_z 
-                print("Scale values changed!")
-                print("Changed row", row)                
-            output.append(list(row.values()))
-        print(len(output))
-        return output
-    except ValueError:
-        print("This row does not exist")
-# scale_xyz(get_rows(), '1', '0')  
-# scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
+    extras = dict(scale_x=scale_x, scale_y=scale_y, scale_z=scale_z)
+    return change_function(rows, selected_row, extras, lambda row: row['parent_id'] == parent)
 
 def print_csv():
     output = scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
-    columns = get_column_names()
-    with open('chin.csv', 'w') as f:
-            f.write(columns)
-            f.write("\n".join([",".join(line) for line in output]))
-print_csv()
+    with open('chin1.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames = get_column_names())
+        writer.writeheader()
+        for line in output:
+            writer.writerow(line)
 
 
-
-
-
-def change_color(reader, selected_row, parent, color_r='0', color_g='255', color_b='0'):
+def change_color(rows, selected_row, parent, color_r='0', color_g='255', color_b='0'):
     """
         Sets the values for scale_x, scale_y and scale_z and parent_id based on input 
         and returns a CSV input with updated data
@@ -73,30 +64,22 @@ def change_color(reader, selected_row, parent, color_r='0', color_g='255', color
         :returns: updated row for CSV file
     
     """
-    try:
-        output = []
-        for row  in reader:
-            if row['id'] == selected_row:
-                if parent == '0':
-                    row['parent_id'] = parent
-                    row['color_r'] = color_r
-                    row['color_g'] = color_g
-                    row['color_b'] = color_b
-                    print("Color values changed!")
-            output.append(row.values())
-        print("Color output length", len(output))
-        return output
-    except ValueError:
-        print("This row does not exist")
+    extras = dict(color_r=color_r, color_g=color_g, color_b=color_b) 
+    return change_function(rows, selected_row, extras, lambda row: row['parent_id'] == parent)
 
-# def main():
+def write_to_file(output_rows, out_file = 'chin1.csv'):
+    with open(out_file, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames = get_column_names())
+        writer.writeheader()
+        for line in output_rows:
+            writer.writerow(line)
 
-# # scale_xyz(get_rows(), '1', '0')  
-# scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
-# change_color 
 
-# def write_list(columns):
-#     with open('column_list.csv', 'w') as f:
-#         for column in columns:
-#                 f.write(column + '\n')
-# write_list(get_column_names())
+def main():
+    scale_list = scale_xyz(get_rows(), '36', '0', scale_x='0.3', scale_y='0.2', scale_z='0.4')
+    # write_to_file(scale_list)
+    write_to_file(change_color(scale_list, '36', '0'))
+
+
+if __name__ == "__main__":
+    main()
