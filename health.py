092aaf9a89692597ac93205a81b90d0b720fb003
Sowmya Aji <sowmya.aji@gmail.com>
2020-03-29 14:49:07 -0400

    Add cleaned up and sorted health_data.csv file

--- health.py
+++ health.py
@@ -1,19 +1,26 @@
+"""
+    Module to get past and future global health spending data [from years 1995 - 2030 ] from the IHME into the correct
+    CSV format with just required columns the for Antz vizualisation
+
+"""
+
 import csv
 
 
 def get_rows(in_file):
     """
         Open csv files and return the data as a list of key:value pair dicts
-        
+
     """
     with open(in_file) as csvfile:
         rows = csv.DictReader(csvfile)
         return list(rows)
 
 
-def get_lat_long(data):
+def add_lat_long(health_data):
     """
-        Attaches the latitude and logitude columns to each row of the input file
+        Attaches the latitude and logitude columns 
+        to each row of the input file
 
         params: takes the input data file as a list of dicts
         returns: a list of dicts with the additional columns
@@ -26,10 +33,11 @@ def get_lat_long(data):
             'latitude': line['Latitude'], 'longitude': line['Longitude']}
     # add Country dict key value pairs to the input data file
     country_coords = []
-    for row in data:
+
+    for row in health_data:
         if row['location_name'] in country_dict:
             row.update(country_dict[row['location_name']])
-        country_coords.append(row)   
+        country_coords.append(row)
     return country_coords
 
 
@@ -39,7 +47,7 @@ def get_column_values(inlist, column_names):
 
         params: input list, the required column names
         returns: dict of each country with all the required column values as a list of values
-    
+
     """
     required_columns = {}
     # to remove all years in the input data beyond 2030.
@@ -50,14 +58,14 @@ def get_column_values(inlist, column_names):
     for row in inlist:
         if row['year'] not in years:
             required_columns.setdefault(row['location_name'], []).append(
-            [(row[key]) if row[key] else '0' for key in column_names])
+                [(row[key]) if row[key] else '0' for key in column_names])
     return required_columns
 
 
 def use_column_names():
     """
         List of column names to be used for final CSV as well as to check against the health_futures csv file
-    
+
     """
     return ['location_id',
             'location_name',
@@ -107,23 +115,24 @@ def old_column_names():
             ]
 
 
-def required_columns():
+def required_columns(data1, data2):
     """
-        Sorts the two data sets and adds them together in the format needed to print
+        Gets and sorts the two data sets and adds them together in the format needed to print
 
-        returns: list of values from both input files
-    
+        params: both data sets are inputted as lists of dictionaries
+        returns: values from both input files as a list
+
     """
-    # Both these data files are read from the line in the file where the country names begin.
-    # These could change depending on the input file. 
-    data1 = get_rows("fin_retro.csv")[254:]
-    data2 = get_rows("health_future.csv")[410:]
-    item1 = get_column_values(get_lat_long(data1), old_column_names())
-    item2 = get_column_values(get_lat_long(data2), use_column_names())
-    lines1 = item1.values()
-    lines2 = item2.values()
+    
+
+    item1 = get_column_values(add_lat_long(data1), old_column_names())
+    item2 = get_column_values(add_lat_long(data2), use_column_names())
+    # double nested loop getting each line for each country as individual lines for the output list
+    lines1 = [entry for country in item1.values() for entry in country]
+    lines2 = [entry for country in item2.values() for entry in country]
     lines = lines1 + lines2
-    return lines 
+    # sorted using location_name as the key
+    return sorted(lines, key=lambda x: x[1])
 
 
 def write_columns(lines, out_file="health_data.csv"):
@@ -131,20 +140,23 @@ def write_columns(lines, out_file="health_data.csv"):
         Outputs the csv file in the required format 
 
         params: gets the data that needs to be outputted as a list and sets a default out_file
-    
+
     """
 
     with open(out_file, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(use_column_names())
-        writer.writerows(lines)
+        writer.writerows([line for line in lines])
 
 
 def main():
     """
         Calls the entire program
     """
-    write_columns(required_columns())
+    # Both input data files are read from the specific lines in the existing files where the country names begin.
+    # These could change depending on the input file.
+    write_columns(required_columns(get_rows("fin_retro.csv")[
+                  254:], get_rows("health_future.csv")[410:]))
 
 
 if __name__ == "__main__":
